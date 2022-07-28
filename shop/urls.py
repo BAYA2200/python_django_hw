ce1f37d8bfbde15462be1c82b984b4d8e3e52a96
@BAYA2200 <bayaman2101@gmail.com>
2022-07-28 18:20:21 +0600

    first commit

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import greetings
+
+from django.urls import path
+
+from . import views
+from .views import greetings, list_item
 
 urlpatterns = [
     path('/shop/greetings', greetings),
+    path('shop/', list_item),
+    path('<int:item_id>/shop/', views.detail_item, name='detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
+from shop.models import Item
 
 
 def greetings(request):
     return HttpResponse("Добро пожаловать в наш магазин!")
+
+
+def list_item(request):
+    item = Item.objects.all()
+    context = {"item": item, }
+    return render(request, "list_item.html", context)
+
+# item.purchase_set.get(id=request.POST['purchase'])
+def detail_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+
+    context = {'item': item}
+    return render(request, 'detail_item.html', context)
